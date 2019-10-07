bb02eba49d3d50d33dfb6971fdaf25a4cfa8063e
Noah Brace <56211149+KevinDOStner@users.noreply.github.com>
2019-10-06 20:56:40 -0500

    Update FlakeyBiscuits.java

--- ValueDetective/src/FlakeyBiscuits.java
+++ ValueDetective/src/FlakeyBiscuits.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FlakeyBiscuits;
 
 import java.io.*;
@@ -33,7 +28,7 @@ public class FlakeyBiscuits {
       
 //  while((br2.readLine()) != null && (br3.readLine()) !=null) {
  String line;
-     while (!match && ((line = br2.readLine()) != null)) {
+     while (!match && (is.read() != -1)) {
              char garfield = (char) br2.read();
              char odie = (char) br3.read();
       
